846795f6b2fd6639054a9b5c6a7ac9b08ec848be
9jsartin <42806630+9jsartin@users.noreply.github.com>
2018-11-29 13:34:19 -0600

    redo

---
+++ tracker.js
@@ -0,0 +1,69 @@
+//Type your code in this function
+function Tracker(){
+
+  let seeds =[];
+  let percent = [];
+
+  //implement all six functions below
+
+function numOfseeds(){
+  let found = 0;
+  for(let i=0; i < seeds.length; i++){
+    if(seeds [i]!== null){
+      found ++;
+    }
+  }
+  return found;
+}
+
+
+} 
+
+function addSeed(s)
+list.push(s);
+return list;
+}
+
+
+
+
+
+function removeSeed(list,name)
+for(let i=0;<list.length; i++){
+  if(list[i]===name){
+    return null
+  }
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+function reportHealth(list)
+let sum=0; i<list.length; i++0{
+  if (list[i]!=0){
+    sum=sum+list[i];
+
+  }
+}
+return sum/list.length;
+
+
+
+
+function update(){
+  Math.random(0,1);
+  for(let i=0;<list.length; i++){
+  if r=Math.random(0,1);
+  if(r> seeds [i]){
+    
+  } 
+  }
+}
---
+++ user.js
@@ -0,0 +1,17 @@
+//Type your code inside this function
+function User(tracker, torrent){
+
+  let myTracker = tracker;
+  let myTorrent = torrent;
+  let torrentFile = [];
+  let torrentTrackerFile=
+  let 
+
+
+
+  //Type all of your code in here.
+
+func
+
+
+}
